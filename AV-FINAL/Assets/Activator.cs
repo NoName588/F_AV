d918ad043a6f7e3730a12ce62f740508e07a5a6f
SanKocho_de_Pollo <84156615+NoName588@users.noreply.github.com>
2023-11-13 16:37:51 -0500

    cambios

---
+++ AV-FINAL/Assets/Activator.cs
@@ -0,0 +1,125 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Activator : MonoBehaviour
+{
+    public List<GameObject> objectsToActivate;  // Lista de objetos que deseas activar/desactivar
+    public List<int> targetNumbers;  // Lista de targetNumbers correspondientes a cada objeto
+    public UDPReciever udpReciever;  // Asegúrate de asignar el componente UDPReceiver
+
+    public List<GameObject> randomObjects;  // Lista de objetos para activar al azar
+    private int selectedRandomObjectIndex = -1;  // Índice del objeto seleccionado al azar
+
+    private int previousNumber = -1;  // Inicializa con un valor que no coincida con ningún número
+
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        int currentNumber = udpReciever.firstNumber;
+
+        if (currentNumber != previousNumber)
+        {
+            // Verificar si el número no coincide con alguno de los 'targetNumbers'
+            bool foundMatch = false;
+            for (int i = 0; i < objectsToActivate.Count; i++)
+            {
+                if (currentNumber == targetNumbers[i])
+                {
+                    objectsToActivate[i].SetActive(true);
+                    foundMatch = true;
+                }
+                else
+                {
+                    objectsToActivate[i].SetActive(false);
+                }
+            }
+
+            if (!foundMatch)
+            {
+                // Si no se encontró una coincidencia, desactivar todos los objetos
+                foreach (GameObject obj in objectsToActivate)
+                {
+                    obj.SetActive(false);
+                }
+            }
+
+            previousNumber = currentNumber;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Activar un objeto al azar y desactivar los demás
+            SelectRandomObject();
+
+            // Determinar el resultado del juego
+            DetermineOutcome(currentNumber, selectedRandomObjectIndex);
+        }
+    }
+
+    private void SelectRandomObject()
+    {
+        // Desactivar todos los objetos al azar
+        foreach (GameObject obj in randomObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        // Seleccionar un objeto al azar
+        selectedRandomObjectIndex = Random.Range(0, randomObjects.Count);
+        if (selectedRandomObjectIndex >= 0 && selectedRandomObjectIndex < randomObjects.Count)
+        {
+            randomObjects[selectedRandomObjectIndex].SetActive(true);
+        }
+
+        // Mostrar el número de la máquina elegido al azar
+        Debug.Log("Número de la máquina es: " + selectedRandomObjectIndex);
+    }
+
+    private void DetermineOutcome(int playerNumber, int machineNumber)
+    {
+        // Definir las reglas del juego
+        if (playerNumber == machineNumber)
+        {
+            Debug.Log("¡Es un empate!");
+        }
+        else
+        {
+            bool playerWins = false;
+            if (playerNumber == 0)
+            {
+                playerWins = machineNumber == 4;
+            }
+            else if (playerNumber == 1)
+            {
+                playerWins = machineNumber == 2 || machineNumber == 4;
+            }
+            else if (playerNumber == 2)
+            {
+                playerWins = machineNumber == 4 || machineNumber == 3;
+            }
+            else if (playerNumber == 3)
+            {
+                playerWins = machineNumber == 1 || machineNumber == 4;
+            }
+            else if (playerNumber == 4)
+            {
+                playerWins = machineNumber == 0;
+            }
+
+            if (playerWins)
+            {
+                Debug.Log("¡Ganaste!");
+            }
+            else
+            {
+                Debug.Log("¡Perdiste!");
+            }
+        }
+    }
+
+}
+
--- AV-FINAL/Assets/UDPReciever.cs
+++ AV-FINAL/Assets/UDPReciever.cs
@@ -7,40 +7,43 @@ using System.Threading;
 
 public class UDPReciever : MonoBehaviour
 {
-
     Thread receiveThread;
     UdpClient client;
     public int port = 5052;
-    public bool startRecieving = true;
+    public bool startReceiving = true;
     public bool printToConsole = false;
     public string data;
-
+    public int firstNumber; // Variable para almacenar el primer número
 
     public void Start()
     {
-
-        receiveThread = new Thread(
-            new ThreadStart(ReceiveData));
+        receiveThread = new Thread(new ThreadStart(ReceiveData));
         receiveThread.IsBackground = true;
         receiveThread.Start();
     }
 
-
     // receive thread
     private void ReceiveData()
     {
-
         client = new UdpClient(port);
-        while (startRecieving)
+        while (startReceiving)
         {
-
             try
             {
                 IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
                 byte[] dataByte = client.Receive(ref anyIP);
                 data = Encoding.UTF8.GetString(dataByte);
 
-                if (printToConsole) { print(data); }
+                // Dividir los datos por punto y coma para obtener el primer número
+                string[] dataParts = data.Split(';');
+                if (dataParts.Length > 0)
+                {
+                    if (int.TryParse(dataParts[0], out firstNumber))
+                    {
+                        if (printToConsole) { print(data); }
+                        // Puedes usar 'firstNumber' para acceder al primer número
+                    }
+                }
             }
             catch (Exception err)
             {
@@ -48,5 +51,5 @@ public class UDPReciever : MonoBehaviour
             }
         }
     }
-
 }
+
